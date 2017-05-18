08d2fccc0b1bc2ad33fd005a53e6b54ba13f317e
Steve Gravrock <sdg@panix.com>
2017-05-18 15:03:56 -0700

    Added demo script

---
+++ demo.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const Router = require("./router");
+
+if (process.argv.length !== 6) {
+	console.error("Usage: ", process.argv0, process.argv[1], "srcLat srcLon destLat destLon");
+	process.exit(1);
+}
+
+const src = {
+	lat: parseFloat(process.argv[2]),
+	lon: parseFloat(process.argv[3]),
+};
+const dest = {
+	lat: parseFloat(process.argv[4]),
+	lon: parseFloat(process.argv[5]),
+};
+
+const router = new Router({key: "test"});
+router.findTrips(src, dest).then(function(trips) {
+	console.log("Found", trips.length, "trips:");
+	trips.forEach((t) => console.log(t.tripId));
+});
