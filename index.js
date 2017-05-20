7def30b3eb4db2315321ee9bd22760c883017fb3
Steve Gravrock <sdg@panix.com>
2017-05-19 17:22:43 -0700

    Simple Node app to output trips between predefined points

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const Router = require("./lib/router");
+
+function require_env(name) {
+	const value = process.env[name];
+
+	if (!value) {
+		throw new Error("The " + name + " environment variable must be set");
+	}
+
+	return value;
+}
+
+const src = {
+	lat: parseFloat(require_env("SRC_LAT")),
+	lon: parseFloat(require_env("SRC_LON")),
+};
+const dest = {
+	lat: parseFloat(require_env("DEST_LAT")),
+	lon: parseFloat(require_env("DEST_LON")),
+};
+const key = require_env("OBA_API_KEY");
+
+app.get('/', function (req, res) {
+	new Router({key: key}).findTrips(src, dest).then(function(trips) {
+		res.send(JSON.stringify(trips));
+	});
+});
+
+const port = parseInt(process.env.PORT || "80", 10);
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
