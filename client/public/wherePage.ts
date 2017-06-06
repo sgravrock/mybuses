5e496ed288218bef6fedc84fae3712e56f710c73
Steve Gravrock <sdg@panix.com>
2017-06-06 10:46:34 -0700

    Don't try to determine the user's location until they ask for it

--- client/public/wherePage.ts
+++ client/public/wherePage.ts
@@ -6,21 +6,36 @@ interface LocationService {
 export function init(root: HTMLElement, locationService: LocationService) {
 	const srcLatField = root.querySelector("[name=srclat]") as HTMLInputElement;
 	const srcLonField = root.querySelector("[name=srclon]") as HTMLInputElement;
+	const useCurrentBtn = root.querySelector(".use-current") as HTMLInputElement;
 
-	locationService.getCurrentPosition(function(result) {
-		srcLatField.value = result.coords.latitude.toString();
-		srcLonField.value = result.coords.longitude.toString();
-	}, function(error) {
-		console.error(error);
+	function enableInputs() {
+		[srcLatField, srcLonField, useCurrentBtn].forEach(b => b.disabled = false);
+	};
 
-		if (error.code === 1) {
-			// User denied permission
-			return;
-		}
+	function disableInputs() {
+		[srcLatField, srcLonField, useCurrentBtn].forEach(b => b.disabled = true);
+	};
 
-		const indicator = document.createElement("p");
-		indicator.className = "error";
-		indicator.textContent = "Could not find your location";
-		root.insertBefore(indicator, root.firstChild);
+	useCurrentBtn.addEventListener("click", function() {
+		disableInputs();
+
+		locationService.getCurrentPosition(function(result) {
+			srcLatField.value = result.coords.latitude.toString();
+			srcLonField.value = result.coords.longitude.toString();
+			enableInputs();
+		}, function(error) {
+			enableInputs();
+			console.error(error);
+	
+			if (error.code === 1) {
+				// User denied permission
+				return;
+			}
+	
+			const indicator = document.createElement("p");
+			indicator.className = "error";
+			indicator.textContent = "Could not find your location";
+			root.insertBefore(indicator, root.firstChild);
+		});
 	});
 }
