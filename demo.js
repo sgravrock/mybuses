29a30a49aceff5345f3133dc7b9e2a537106078b
Steve Gravrock <sgravrock@pivotal.io>
2017-06-26 17:19:08 -0700

    Demo script pulls endpoints from env if unspecified

--- demo.js
+++ demo.js
@@ -1,19 +1,31 @@
 #!/usr/bin/env node
 const Router = require("./lib/router").Router;
 
-if (process.argv.length !== 6) {
-	console.error("Usage: ", process.argv0, process.argv[1], "srcLat srcLon destLat destLon");
+let src, dest;
+
+if (process.argv.length === 6) {
+	src = {
+		lat: parseFloat(process.argv[2]),
+		lon: parseFloat(process.argv[3]),
+	};
+	dest = {
+		lat: parseFloat(process.argv[4]),
+		lon: parseFloat(process.argv[5]),
+	};
+} else if (process.argv.length === 2) {
+	src = {
+		lat: parseFloat(process.env["SRC_LAT"]),
+		lon: parseFloat(process.env["SRC_LON"]),
+	};
+	dest = {
+		lat: parseFloat(process.env["DEST_LAT"]),
+		lon: parseFloat(process.env["DEST_LON"]),
+	};
+} else {
+	console.error("Usage: ", process.argv0, process.argv[1], "[srcLat srcLon destLat destLon]");
 	process.exit(1);
 }
 
-const src = {
-	lat: parseFloat(process.argv[2]),
-	lon: parseFloat(process.argv[3]),
-};
-const dest = {
-	lat: parseFloat(process.argv[4]),
-	lon: parseFloat(process.argv[5]),
-};
 
 const router = new Router({key: "test"});
 router.findTrips(src, dest).then(function(trips) {
