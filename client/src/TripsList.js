29b5d03196a9823808fff7c8bb4fafdc468a4008
Steve Gravrock <sdg@panix.com>
2018-05-06 14:21:37 -0700

    Added the fine print

--- client/src/TripsList.js
+++ client/src/TripsList.js
@@ -4,20 +4,39 @@ import './TripsList.css';
 
 export function TripsList(props) {
 	return (
-		<ol className="TripsList">
-			{props.trips.map(trip => (
-				<li className="TripsList-item" key={trip.tripId}>
-					{trip.route.shortName} from {trip.srcStop.name}
-					<div className="TripsList-time">
-						in {trip.srcStop.arrivalTime.minutesUntil} minutes
-						{trip.srcStop.arrivalTime.isScheduled ? '*' : ''}
-					</div>
-					<div className="TripsList-time">
-						Arrive at {formatTime(trip.destStop.arrivalTime.date)}
-						{trip.destStop.arrivalTime.isScheduled ? '*' : ''}
-					</div>
-				</li>
-			))}
-		</ol>
+		<div className="TripsList">
+			<ol>
+				{props.trips.map(trip => (
+					<li className="TripsList-item" key={trip.tripId}>
+						{trip.route.shortName} from {trip.srcStop.name}
+						<div className="TripsList-time">
+							in {trip.srcStop.arrivalTime.minutesUntil} minutes
+							{trip.srcStop.arrivalTime.isScheduled ? '*' : ''}
+						</div>
+						<div className="TripsList-time">
+							Arrive at {formatTime(trip.destStop.arrivalTime.date)}
+							{trip.destStop.arrivalTime.isScheduled ? '*' : ''}
+						</div>
+					</li>
+				))}
+			</ol>
+			<p>
+				* Denotes a scheduled time for which no vehicle status is
+				available.
+			</p>
+			<p>
+				The fine print: Mybuses won't find options that arrive more
+				than two hours after departure or that involve more than one
+				route. Results are (at best) only as accurate as the data
+				provided by One Bus Away. If this software makes you late for
+				work, we'll feel a little bad about it but accept no
+				responsibility. This software was produced in a facility that
+				also processes wheat, dairy, and peanuts. Offer void where
+				prohibited by law. For external use only. Discontinue use and
+				consult a doctor if you develop a rash or fever. Do not fold,
+				spindle, or mutilate. Do not taunt Happy Fun Website.
+			</p>
+		
+		</div>
 	);
 }
