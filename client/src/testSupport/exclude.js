8c303cd37f4d5b561ecacf2616dce02d975a08a2
Steve Gravrock <sdg@panix.com>
2018-05-31 20:54:18 -0700

    Use ignore-styles instead of require-hacker

--- client/src/testSupport/exclude.js
+++ client/src/testSupport/exclude.js
@@ -1,4 +1,1 @@
-import requireHacker from 'require-hacker'
-requireHacker.hook('css', () => 'module.exports = ""');
-requireHacker.hook('svg', () => 'module.exports = ""');
-requireHacker.hook('png', () => 'module.exports = ""');
+require('ignore-styles');
