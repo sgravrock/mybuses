87d35d5cd44192733b4c3b301a288e69190d40cd
Steve Gravrock <sdg@panix.com>
2018-10-27 19:56:30 -0700

    Converted TripsContainer to use the withContext hook

--- src/TripsContainer.tsx
+++ src/TripsContainer.tsx
@@ -1,24 +1,21 @@
 import * as React from 'react';
 // @ts-ignore
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useContext} from 'react';
 import * as PropTypes from 'prop-types';
 import {Trip} from './trips';
-import {IDefaultRouter, DefaultRouterContext, DefaultRouter} from "./routing/default-router";
+import {IDefaultRouter, DefaultRouterContext} from "./routing/default-router";
 
-interface OuterProps {
+interface Props {
 	render: (trips: Trip[]) => JSX.Element;
 }
 
-interface Props extends OuterProps {
-	apiClient: IDefaultRouter;
-}
-
-const InnerTripsContainer: React.SFC<Props> = (props) => {
+const TripsContainer: React.SFC<Props> = (props) => {
 	const [loadingFailed, setLoadingFailed] = useState(false);
 	const [trips, setTrips] = useState(null);
+	const router: IDefaultRouter = useContext(DefaultRouterContext);
 
 	useEffect(() => {
-		props.apiClient.trips()
+		router.trips()
 			.then(
 				setTrips,
 				() => setLoadingFailed(true)
@@ -32,20 +29,10 @@ const InnerTripsContainer: React.SFC<Props> = (props) => {
 	} else {
 		return <div>Searching for trips...</div>;
 	}
-
-
 };
 
-InnerTripsContainer.propTypes = {
+TripsContainer.propTypes = {
 	render: PropTypes.func.isRequired,
-	// TODO this is wrong, but at least it compiles.
-	apiClient: PropTypes.instanceOf(DefaultRouter).isRequired
 };
 
-export const TripsContainer: React.SFC<OuterProps> = (props) => {
-	return (
-		<DefaultRouterContext.Consumer>
-			{apiClient => <InnerTripsContainer {...props} apiClient={apiClient}/>}
-		</DefaultRouterContext.Consumer>
-	)
-};
+export {TripsContainer};
